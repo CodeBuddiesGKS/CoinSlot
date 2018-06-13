8dcefc343da7d35b04e676a77adfca13fc23ace3
gavinsnigums <gavinsnigums@gmail.com>
2018-06-13 15:32:46 -0500

    Added the Compositor and layers. Added the animation frame. Added an army of Marios.

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,26 @@
+import Compositor from './Compositor.js';
+import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 import {loadLevel} from './loaders.js';
 import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
-
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
 
 const canvas = document.getElementById("coinSlot");
 const context = canvas.getContext("2d");
 
 Promise.all([loadMarioSprite(), loadBackgroundSprites(), loadLevel('1-1')])
-.then(([marioSprites, sprites, level]) => {
-    level.backgrounds.forEach(background => {
-        drawBackground(background, context, sprites);
-    });
-    marioSprites.draw('idle', context, 64, 192);
+.then(([marioSprite, backgroundSprites, level]) => {
+    const comp = new Compositor();
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+    comp.layers.push(backgroundLayer);
+
+    const pos = { x: 16, y: 192 };
+
+    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    comp.layers.push(spriteLayer);
+
+    function update() {
+        comp.draw(context);
+        pos.x += 2;
+        requestAnimationFrame(update);
+    }
+    update();
 });
